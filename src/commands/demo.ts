4001ac99e8b82416ece6ab602eb3f914dd9bc5dd
Jacob Gold <goldjacobe@gmail.com>
2022-08-18 16:04:23 +0000

    refactor: remove runCommand dependency in gt demo (#1265)

--- src/commands/demo.ts
+++ src/commands/demo.ts
@@ -3,7 +3,6 @@ import yargs from 'yargs';
 import { graphiteWithoutRepo } from '../lib/runner';
 import { GitRepo } from '../lib/utils/git_repo';
 import { makeId } from '../lib/utils/make_id';
-import { runCommand } from '../lib/utils/run_command';
 
 export const command = 'demo';
 export const canonical = 'demo';
@@ -18,107 +17,71 @@ export const handler = async (argv: argsT): Promise<void> => {
     const tmpDir = tmp.dirSync();
     context.splog.info(tmpDir.name);
     const repo = new GitRepo(tmpDir.name);
-
-    const id = makeId(4);
+    const id = makeId(8);
 
     repo.createChangeAndCommit('First commit');
     repo.createChangeAndCommit('Second commit');
 
+    repo.runCliCommand(['repo', 'init', `--no-interactive`]);
+
     repo.createChange('[Product] Add review queue filter api');
-    runCliCommand(
-      [
-        'branch',
-        'create',
-        `tr-${id}--review_queue_api`,
-        '-m',
-        '[Product] Add review queue filter api',
-      ],
-      tmpDir.name
-    );
+    repo.runCliCommand([
+      'branch',
+      'create',
+      `${id}-review_queue_api`,
+      '-m',
+      '[Product] Add review queue filter api',
+    ]);
 
     repo.createChange('[Product] Add review queue filter server');
-    runCliCommand(
-      [
-        'branch',
-        'create',
-        `tr-${id}--review_queue_server`,
-        '-m',
-        '[Product] Add review queue filter server',
-      ],
-      tmpDir.name
-    );
+    repo.runCliCommand([
+      'branch',
+      'create',
+      `${id}-review_queue_server`,
+      '-m',
+      '[Product] Add review queue filter server',
+    ]);
 
     repo.createChange('[Product] Add review queue filter frontend');
-    runCliCommand(
-      [
-        'branch',
-        'create',
-        `tr-${id}--review_queue_frontend`,
-        '-m',
-        '[Product] Add review queue filter frontend',
-      ],
-      tmpDir.name
-    );
+    repo.runCliCommand([
+      'branch',
+      'create',
+      `${id}-review_queue_frontend`,
+      '-m',
+      '[Product] Add review queue filter frontend',
+    ]);
 
     repo.checkoutBranch('main');
 
     repo.createChange('[Bug Fix] Fix crashes on reload');
-    runCliCommand(
-      [
-        'branch',
-        'create',
-        `tr-${id}--fix_crash_on_reload`,
-        '-m',
-        '[Bug Fix] Fix crashes on reload',
-      ],
-      tmpDir.name
-    );
+    repo.runCliCommand([
+      'branch',
+      'create',
+      `${id}-fix_crash_on_reload`,
+      '-m',
+      '[Bug Fix] Fix crashes on reload',
+    ]);
 
     repo.checkoutBranch('main');
 
     repo.createChange('[Bug Fix] Account for empty state');
-    runCliCommand(
-      [
-        'branch',
-        'create',
-        `tr-${id}--account_for_empty_state`,
-        '-m',
-        '[Bug Fix] Account for empty state',
-      ],
-      tmpDir.name
-    );
+    repo.runCliCommand([
+      'branch',
+      'create',
+      `${id}-account_for_empty_state`,
+      '-m',
+      '[Bug Fix] Account for empty state',
+    ]);
 
     repo.checkoutBranch('main');
 
-    runCommand({
-      command: 'git',
-      args: [
-        'remote',
-        'add',
-        'origin',
-        'git@github.com:withgraphite/graphite-demo-repo.git',
-      ],
-      options: { cwd: tmpDir.name },
-      onError: 'throw',
-    });
+    repo.runGitCommand([
+      'remote',
+      'add',
+      'origin',
+      'git@github.com:withgraphite/graphite-demo-repo.git',
+    ]);
 
-    runCommand({
-      command: 'git',
-      args: ['push', 'origin', 'main', '-f'],
-      options: { cwd: tmpDir.name },
-      onError: 'throw',
-    });
+    repo.runGitCommand(['push', 'origin', 'main', '-f']);
   });
 };
-
-function runCliCommand(args: string[], fromDir: string) {
-  runCommand({
-    command: 'gt',
-    args,
-    options: {
-      stdio: 'inherit',
-      cwd: fromDir,
-    },
-    onError: 'throw',
-  });
-}
